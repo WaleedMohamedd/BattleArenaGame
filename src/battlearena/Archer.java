a0be42975beaaf06b7f389a9e908c1f24135e24c
WaleedMohamedd <walid.tawfik2005@gmail.com>
2025-12-18 00:13:02 +0200

    Archer.java

--- src/battlearena/Archer.java
+++ src/battlearena/Archer.java
@@ -5,13 +5,7 @@ import javafx.scene.paint.Color;
 public class Archer extends Fighter {
 
     public Archer(int playerNumber) {
-        super(
-                "Archer",
-                80,
-                5.0,
-                playerNumber == 1 ? Color.DARKGREEN : Color.FORESTGREEN,
-                playerNumber
-        );
+        super("Archer", 80, 5.0, playerNumber == 1 ? Color.DARKGREEN : Color.FORESTGREEN, playerNumber);
     }
 
     @Override
