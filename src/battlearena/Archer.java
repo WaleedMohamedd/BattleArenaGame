610b6e26f93449503730c4f1b37c6aef8040a570
Waleedd <walid.tawfik2005@gmail.com>
2025-12-17 16:54:51 +0200

    Add Archer character

---
+++ src/battlearena/Archer.java
@@ -0,0 +1,22 @@
+package battlearena;
+
+import javafx.scene.paint.Color;
+
+public class Archer extends Fighter {
+
+    public Archer(int playerNumber) {
+        super(
+                "Archer",
+                80,
+                5.0,
+                playerNumber == 1 ? Color.DARKGREEN : Color.FORESTGREEN,
+                playerNumber
+        );
+    }
+
+    @Override
+    protected void initializeWeapons() {
+        addWeapon(new Bow());
+        addWeapon(new Pistol());
+    }
+}
