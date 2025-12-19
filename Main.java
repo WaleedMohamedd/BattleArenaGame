01d753128cea77d0ccd59446c0cfcfbb7720a968
WaleedMohamedd <walid.tawfik2005@gmail.com>
2025-12-19 22:06:00 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+package battlearena;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(BattleArenaApp.class, args);
+    }
+}
